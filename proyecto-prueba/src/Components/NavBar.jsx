98f726a90dee81a0c76ab243de284b6b47dbeff3
aferreira <alan.f.ferreira89@gmail.com>
2022-04-26 00:53:15 -0300

    NavBar

---
+++ proyecto-prueba/src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(props) {
+    return (
+        <div>
+            <navbar >
+                <container className='nav'>
+                    <h1>E-COMMERCE</h1>
+                    <a href="">Opción1</a>
+                    <a href="">Opción1</a>
+                    <a href="">Opción1</a>
+                </container>
+            </navbar>
+        </div>
+    );
+}
+
+export default NavBar;
